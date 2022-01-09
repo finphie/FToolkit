c1fe1009a6e77dc70d1ddc20abf40f54ac8a6c28
finphie <19237170+finphie@users.noreply.github.com>
2022-01-09 17:01:21 +0900

    初回コミット

---
+++ Source/FToolkit.Abstractions/IO/IFileOperations.cs
@@ -0,0 +1,41 @@
+﻿namespace FToolkit.IO;
+
+/// <summary>
+/// ファイル操作に関する定義です。
+/// </summary>
+public interface IFileOperations
+{
+    /// <summary>
+    /// 指定されたデータをファイルに保存します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <param name="bytes">データ</param>
+    void Create(string filePath, ReadOnlySpan<byte> bytes);
+
+    /// <summary>
+    /// 指定されたデータをファイルに保存します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <param name="bytes">データ</param>
+    void Create(string filePath, ReadOnlyMemory<byte> bytes);
+
+    /// <summary>
+    /// 指定されたデータをファイルに保存します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <param name="bytes">データ</param>
+    void Save(string filePath, ReadOnlySpan<byte> bytes);
+
+    /// <summary>
+    /// 指定されたデータをファイルに保存します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <param name="bytes">データ</param>
+    void Save(string filePath, ReadOnlyMemory<byte> bytes);
+
+    /// <summary>
+    /// 指定されたファイルを削除します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    void Delete(string filePath);
+}
