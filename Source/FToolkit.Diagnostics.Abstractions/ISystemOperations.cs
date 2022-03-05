54aa750a9662deb44bb02c84865d9a5e2a1070cd
finphie <19237170+finphie@users.noreply.github.com>
2022-03-05 12:57:25 +0900

    コメント追加

--- Source/FToolkit.Diagnostics.Abstractions/ISystemOperations.cs
+++ Source/FToolkit.Diagnostics.Abstractions/ISystemOperations.cs
@@ -16,7 +16,7 @@ public interface ISystemOperations
     /// <summary>
     /// 指定されたファイルを実行します。
     /// </summary>
-    /// <param name="bufferWriter">バッファ</param>
+    /// <param name="bufferWriter">バッファーライター</param>
     /// <param name="fileName">ファイル名</param>
     /// <param name="arguments">引数</param>
     /// <param name="environmentVariable">環境変数</param>
