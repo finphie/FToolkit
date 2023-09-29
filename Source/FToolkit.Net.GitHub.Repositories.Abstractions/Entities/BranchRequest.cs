dfda2bc281e181305d1bcb8aeb84ea0f53727435
finphie <19237170+finphie@users.noreply.github.com>
2023-09-30 08:00:05 +0900

    リファクタリング

--- Source/FToolkit.Net.GitHub.Repositories.Abstractions/Entities/BranchRequest.cs
+++ Source/FToolkit.Net.GitHub.Repositories.Abstractions/Entities/BranchRequest.cs
@@ -6,8 +6,8 @@ namespace FToolkit.Net.GitHub.Repositories.Entities;
 /// <summary>
 /// ブランチ情報を表すクラスです。
 /// </summary>
-/// <param name="Owner">オーナー名。</param>
-/// <param name="Name">リポジトリ名。</param>
-/// <param name="Branch">ブランチ名。</param>
-/// <param name="Settings">設定。</param>
+/// <param name="Owner">オーナー名</param>
+/// <param name="Name">リポジトリ名</param>
+/// <param name="Branch">ブランチ名</param>
+/// <param name="Settings">設定</param>
 public sealed record BranchRequest(string Owner, string Name, string Branch, GitHubBranchProtection? Settings = null) : IAggregateRoot;
--- Source/FToolkit.Net.GitHub.Repositories.Abstractions/Entities/Request.cs
+++ Source/FToolkit.Net.GitHub.Repositories.Abstractions/Entities/Request.cs
@@ -6,7 +6,7 @@ namespace FToolkit.Net.GitHub.Repositories.Entities;
 /// <summary>
 /// リクエスト情報を表すクラスです。
 /// </summary>
-/// <param name="Owner">オーナー名。</param>
-/// <param name="Name">リポジトリ名。</param>
-/// <param name="Settings">設定。</param>
+/// <param name="Owner">オーナー名</param>
+/// <param name="Name">リポジトリ名</param>
+/// <param name="Settings">設定</param>
 public sealed record Request(string Owner, string Name, GitHubRepository Settings) : IAggregateRoot;
