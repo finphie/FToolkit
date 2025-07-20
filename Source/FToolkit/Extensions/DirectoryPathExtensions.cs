69590a5b04619d8924a0bca5950283d7949570be
finphie <19237170+finphie@users.noreply.github.com>
2025-07-20 20:30:12 +0900

    アナライザーが例外を出すのでその対応

--- Source/FToolkit/Extensions/DirectoryPathExtensions.cs
+++ Source/FToolkit/Extensions/DirectoryPathExtensions.cs
@@ -7,15 +7,11 @@ namespace FToolkit.Extensions;
 /// </summary>
 public static class DirectoryPathExtensions
 {
-#pragma warning disable CA1034, CS1591, CA1822
-    extension(DirectoryPath directoryPath)
-#pragma warning restore CA1034, CS1591
-    {
-        /// <summary>
-        /// ディレクトリのパスからディレクトリ名を取得します。
-        /// </summary>
-        /// <returns>現在のパスから抽出したディレクトリ名を表す<see cref="DirectoryName"/>オブジェクト。</returns>
-        public DirectoryName GetDirectoryName()
+    /// <summary>
+    /// ディレクトリのパスからディレクトリ名を取得します。
+    /// </summary>
+    /// <param name="directoryPath">ディレクトリのパス</param>
+    /// <returns>現在のパスから抽出したディレクトリ名を表す<see cref="DirectoryName"/>オブジェクト。</returns>
+    public static DirectoryName GetDirectoryName(this DirectoryPath directoryPath)
             => new(Path.GetDirectoryName(directoryPath.AsPrimitive()));
-    }
 }
--- Source/FToolkit/Extensions/FilePathExtensions.cs
+++ Source/FToolkit/Extensions/FilePathExtensions.cs
@@ -7,22 +7,19 @@ namespace FToolkit.Extensions;
 /// </summary>
 public static class FilePathExtensions
 {
-#pragma warning disable CA1034, CS1591, CA1822
-    extension(FilePath filePath)
-#pragma warning restore CA1034, CS1591
-    {
-        /// <summary>
-        /// ファイルパスからファイル名を取得します。
-        /// </summary>
-        /// <returns>現在のパスから抽出したファイル名を表す<see cref="FileName"/>オブジェクト。</returns>
-        public FileName GetFileName()
-            => new(Path.GetFileName(filePath.AsPrimitive()));
+    /// <summary>
+    /// ファイルパスからファイル名を取得します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <returns>現在のパスから抽出したファイル名を表す<see cref="FileName"/>オブジェクト。</returns>
+    public static FileName GetFileName(this FilePath filePath)
+        => new(Path.GetFileName(filePath.AsPrimitive()));
 
-        /// <summary>
-        /// ファイルパスに拡張子が含まれているかどうかを判定します。
-        /// </summary>
-        /// <returns>拡張子が含まれている場合は<see langword="true"/>、含まれていない場合は<see langword="false"/>を返します。</returns>
-        public bool HasExtension()
-            => Path.HasExtension(filePath.AsPrimitive());
-    }
+    /// <summary>
+    /// ファイルパスに拡張子が含まれているかどうかを判定します。
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <returns>拡張子が含まれている場合は<see langword="true"/>、含まれていない場合は<see langword="false"/>を返します。</returns>
+    public static bool HasExtension(this FilePath filePath)
+        => Path.HasExtension(filePath.AsPrimitive());
 }
