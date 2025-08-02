9d50b980b500d50a34441481453fcf3a6b8d2610
finphie <19237170+finphie@users.noreply.github.com>
2025-08-02 23:07:11 +0900

    Update ViewLocator.cs

--- Source/FToolkit/Views/ViewLocator.cs
+++ Source/FToolkit/Views/ViewLocator.cs
@@ -10,20 +10,20 @@ namespace FToolkit.Views;
 public sealed partial class ViewLocator : IViewLocator
 {
     readonly ILogger<ViewLocator> _logger;
-    readonly IServiceProvider _services;
+    readonly IServiceProvider _provider;
 
     /// <summary>
     /// <see cref="ViewLocator"/>クラスの新しいインスタンスを初期化します。
     /// </summary>
     /// <param name="logger">ログ記録のためのオブジェクト</param>
-    /// <param name="services">Viewの依存関係解決に使用するサービスプロバイダー</param>
-    public ViewLocator(ILogger<ViewLocator> logger, IServiceProvider services)
+    /// <param name="provider">Viewの依存関係解決に使用するサービスプロバイダー</param>
+    public ViewLocator(ILogger<ViewLocator> logger, IServiceProvider provider)
     {
         ArgumentNullException.ThrowIfNull(logger);
-        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(provider);
 
         _logger = logger;
-        _services = services;
+        _provider = provider;
     }
 
     /// <inheritdoc/>
@@ -31,7 +31,7 @@ public sealed partial class ViewLocator : IViewLocator
         where T : class, IViewModel
     {
         LogViewGetting();
-        return _services.GetRequiredService<IViewFor<T>>();
+        return _provider.GetRequiredService<IViewFor<T>>();
     }
 
     [LoggerMessage(Level = LogLevel.Information, Message = "Getting the View.")]
