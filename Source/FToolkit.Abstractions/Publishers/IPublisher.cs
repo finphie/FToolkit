c4f1280e7786af4d88bf4c10b73cdabcee48ca50
finphie <19237170+finphie@users.noreply.github.com>
2025-08-04 20:05:33 +0900

    Publisher

---
+++ Source/FToolkit.Abstractions/Publishers/IPublisher.cs
@@ -0,0 +1,14 @@
+﻿namespace FToolkit.Publishers;
+
+/// <summary>
+/// パブリッシャーのインターフェイスです。
+/// </summary>
+public interface IPublisher
+{
+    /// <summary>
+    /// 指定したメッセージを発行します。
+    /// </summary>
+    /// <typeparam name="T">発行するメッセージの型</typeparam>
+    /// <param name="message">発行するメッセージ</param>
+    void Publish<T>(T message);
+}
