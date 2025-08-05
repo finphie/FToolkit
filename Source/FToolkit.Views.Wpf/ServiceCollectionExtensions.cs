a94c849a0d057573a064c3597ec8e0a21e490e26
finphie <19237170+finphie@users.noreply.github.com>
2025-08-05 21:12:56 +0900

    ServiceCollectionExtensions

---
+++ Source/FToolkit.Views.Wpf/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FToolkit.Views.Wpf;
+
+/// <summary>
+/// WPFに関連するクラスを<see cref="IServiceCollection"/>に追加する拡張メソッドです。
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// WPFに関連するオブジェクトを<see cref="IServiceCollection"/>に追加します。
+    /// </summary>
+    /// <param name="services">追加する対象の<see cref="IServiceCollection"/></param>
+    public static void AddWpf(this IServiceCollection services)
+        => services.AddSingleton<IWindowService, WpfWindowService>();
+}
