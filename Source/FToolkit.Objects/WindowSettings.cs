4efb8c2218af0bb9901d55fc3653f2dfbbfd5a38
finphie <19237170+finphie@users.noreply.github.com>
2025-08-13 14:10:20 +0900

    設定

---
+++ Source/FToolkit.Objects/WindowSettings.cs
@@ -0,0 +1,37 @@
+﻿namespace FToolkit.Objects;
+
+/// <summary>
+/// Window設定を表すクラスです。
+/// </summary>
+public sealed record WindowSettings
+{
+    /// <summary>
+    /// 幅
+    /// </summary>
+    public int Width { get; set; } = 1200;
+
+    /// <summary>
+    /// 高さ
+    /// </summary>
+    public int Height { get; set; } = 600;
+
+    /// <summary>
+    /// X座標
+    /// </summary>
+    public int Left { get; set; } = -1;
+
+    /// <summary>
+    /// Y座標
+    /// </summary>
+    public int Top { get; set; } = -1;
+
+    /// <summary>
+    /// 状態
+    /// </summary>
+    public WindowState State { get; set; } = WindowState.Normal;
+
+    /// <summary>
+    /// 最前面にするかどうか
+    /// </summary>
+    public bool IsAlwaysOnTop { get; set; }
+}
---
+++ Source/FToolkit.Objects/WindowState.cs
@@ -0,0 +1,22 @@
+﻿namespace FToolkit.Objects;
+
+/// <summary>
+/// Windowの状態を表す列挙型です。
+/// </summary>
+public enum WindowState
+{
+    /// <summary>
+    /// 通常
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// 最小化
+    /// </summary>
+    Minimized,
+
+    /// <summary>
+    /// 最大化
+    /// </summary>
+    Maximized
+}
