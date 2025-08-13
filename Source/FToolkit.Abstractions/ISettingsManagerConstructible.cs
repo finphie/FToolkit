dd4f7255e131652b25fe3f8dc7322cac769183a6
finphie <19237170+finphie@users.noreply.github.com>
2025-08-13 16:48:59 +0900

    x

---
+++ Source/FToolkit.Abstractions/ISettingsManagerConstructible.cs
@@ -0,0 +1,12 @@
+﻿using FToolkit.Options;
+using FToolkit.Publishers;
+using FToolkit.ViewModels;
+
+namespace FToolkit;
+
+/// <summary>
+/// 設定マネージャーのインスタンスを生成するためのインターフェイスです。
+/// </summary>
+/// <typeparam name="TSelf">自身の型</typeparam>
+/// <typeparam name="TSettings">設定の型</typeparam>
+public interface ISettingsManagerConstructible<out TSelf, in TSettings> : IConstructible<TSelf, IReloadableOptions<TSettings>, IPublisher, IViewModel>;
--- Source/FToolkit.Abstractions/Managers/IApplicationSettingsManagerBase.cs
+++ Source/FToolkit.Abstractions/Managers/IApplicationSettingsManagerBase.cs
@@ -21,3 +21,12 @@ public interface IApplicationSettingsManagerBase<out TSettings> : ISettingsManag
     /// <param name="windowState">Windowの状態</param>
     void Notify(WindowState windowState);
 }
+
+/// <summary>
+/// 自身のインスタンスを生成できる、アプリケーション設定マネージャーの基本インターフェイスです。
+/// </summary>
+/// <typeparam name="TSettings">設定の型</typeparam>
+/// <typeparam name="TSelf">自身の型</typeparam>
+public interface IApplicationSettingsManagerBase<TSettings, out TSelf> : IApplicationSettingsManagerBase<TSettings>, ISettingsManagerConstructible<TSelf, TSettings>
+    where TSettings : ApplicationSettingsBase
+    where TSelf : IApplicationSettingsManagerBase<TSettings, TSelf>;
