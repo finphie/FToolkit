f04a33e585e62ebec39d8937eeda5250a7072cc2
finphie <19237170+finphie@users.noreply.github.com>
2025-08-14 21:03:25 +0900

    Create ChangeWindowStateSubscriber.cs

---
+++ Source/FToolkit/Subscribers/ChangeWindowStateSubscriber.cs
@@ -0,0 +1,30 @@
+﻿using FToolkit.Commands;
+using FToolkit.Views;
+using ZeroMessenger;
+
+namespace FToolkit.Subscribers;
+
+/// <summary>
+/// Windowの状態変更コマンドを受信し、Windowの状態を変更するクラスです。
+/// </summary>
+sealed class ChangeWindowStateSubscriber : IDisposable
+{
+    readonly IDisposable _disposable;
+
+    /// <summary>
+    /// <see cref="ChangeWindowStateSubscriber"/>クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="subscriber">Windowの状態変更コマンドを受信するためのオブジェクト</param>
+    /// <param name="windowService">Window関連操作を行うオブジェクト</param>
+    public ChangeWindowStateSubscriber(IMessageSubscriber<ChangeWindowStateCommand> subscriber, IWindowService windowService)
+    {
+        ArgumentNullException.ThrowIfNull(subscriber);
+        ArgumentNullException.ThrowIfNull(windowService);
+
+        _disposable = subscriber.Subscribe(x => windowService.ChangeWindowState(x.ViewModel, x.State));
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+        => _disposable.Dispose();
+}
