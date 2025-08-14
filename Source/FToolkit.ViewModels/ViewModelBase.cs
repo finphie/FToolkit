123b51bc9e8e6f8268a92ffc91b357c9d8780962
finphie <19237170+finphie@users.noreply.github.com>
2025-08-14 21:15:56 +0900

    Update ViewModelBase.cs

--- Source/FToolkit.ViewModels/ViewModelBase.cs
+++ Source/FToolkit.ViewModels/ViewModelBase.cs
@@ -8,26 +8,26 @@ namespace FToolkit.ViewModels;
 /// <summary>
 /// ViewModelの基底クラスです。
 /// </summary>
-public abstract class ViewModelBase : ObservableObject
+public abstract class ViewModelBase : ObservableObject, IViewModel
 {
     /// <summary>
     /// <see cref="ViewModelBase"/>クラスの新しいインスタンスを初期化します。
     /// </summary>
     protected ViewModelBase()
     {
-        SettingsManager = Ioc.Default.GetRequiredService<ISettingsManagerBase<ApplicationSettingsBase>>();
         ApplicationInfo = Ioc.Default.GetRequiredService<ApplicationInfoBase>();
+        SettingsManager = Ioc.Default.GetRequiredService<IApplicationSettingsManagerBase<ApplicationSettingsBase>>();
     }
-
-    /// <summary>
-    /// 設定マネージャーを取得します。
-    /// </summary>
-    protected virtual ISettingsManagerBase<ApplicationSettingsBase> SettingsManager { get; }
 
     /// <summary>
     /// アプリケーション情報を取得します。
     /// </summary>
     protected virtual ApplicationInfoBase ApplicationInfo { get; }
+
+    /// <summary>
+    /// 設定マネージャーを取得します。
+    /// </summary>
+    protected virtual IApplicationSettingsManagerBase<ApplicationSettingsBase> SettingsManager { get; }
 
     /// <summary>
     /// 初期化処理を行います。
